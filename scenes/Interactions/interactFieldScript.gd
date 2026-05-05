56b88ab8a7495eef85072d9d18a8fa9af418d3dc
codingcats <eli.jguzman360@gmail.com>
2026-05-05 09:59:14 -0600

    Interaction fields

---
+++ scenes/Interactions/interactFieldScript.gd
@@ -0,0 +1,15 @@
+extends Area3D
+class_name InteractionField
+
+@export var action_name: String = "interact"
+
+var interact: Callable = func():
+	pass
+
+
+func _on_body_entered(body: Node3D) -> void:
+	InteractionManager.register_area(self)
+
+
+func _on_body_exited(body: Node3D) -> void:
+	InteractionManager.unregister_area(self)
