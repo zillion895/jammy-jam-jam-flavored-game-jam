59e762bcdeaad7b4f35bce5dcfeaf8d65050f463
jack kagie <zillion895@example.com>
2026-05-11 09:31:53 -0600

    feat:door

--- scenes/door/Door.cs
+++ scenes/door/Door.cs
@@ -1,16 +1,42 @@
 using Godot;
 using System;
+using System.Numerics;
 
 public partial class Door : Node3D
 {
-	// Called when the node enters the scene tree for the first time.
-	
+	private Node3D door;
+	private Node3D endPoint;
+	private Godot.Vector3 startPoint;
+	private Node interactField;
+
 	public override void _Ready()
 	{
+		door = GetNode<Node3D>("model");
+		endPoint = GetNode<Node3D>("endPoint");
+		startPoint = door.Position;
+
+		interactField = GetNode("InteractField");
+		if (interactField == null)
+		{
+			GD.PrintErr("Door: InteractField node not found.");
+			return;
+		}
+
+		interactField.Set("interact", Callable.From(Open));
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	public void Open()
 	{
+		GD.Print("open");
+		Tween tween = CreateTween();
+		tween.TweenProperty(door, "position", endPoint.Position, 1f).SetTrans(Tween.TransitionType.Sine).SetEase(Tween.EaseType.In);
+		tween.TweenCallback(Callable.From(() => close())).SetDelay(2f);
+	}
+
+	public void close()
+	{
+		Tween tween = CreateTween();
+		tween.TweenProperty(door, "position", startPoint, 1f).SetTrans(Tween.TransitionType.Sine).SetEase(Tween.EaseType.In);
 	}
 }
+		
