e9c0d795e324c59c1e6eddd674bd3b899d9f45dc
codingcats <eli.jguzman360@gmail.com>
2026-05-15 09:46:14 -0600

    speed change for jail

--- scenes/jail/test_script.gd
+++ scenes/jail/test_script.gd
@@ -1,6 +1,6 @@
 extends Node3D
 
-@export var speed: Vector3 = Vector3(10, 50, 10)
+@export var speed: Vector3 = Vector3(50, 50, 50)
 
 func _process(delta: float) -> void:
 	rotation_degrees += speed * delta
